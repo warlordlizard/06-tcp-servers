d75716de904101d1489e7885d2a5f2dfaacb4cde
warlordlizard <alexjbowers88@gmail.com>
2018-02-21 13:51:49 -0800

    as far as demo was

---
+++ lab-alex/server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = require('./lib/client.js');
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+const ee = new EE();
+
+const pool = [];
+
+ee.on('@all', function(client, string) {
+  pool.forEach( c => {
+    c.socket.write(`${client.nickname}: ${string}`);
+  });
+});
+
+ee.on('@dm', function(cleint, string) {
+  var nickname = string.split(' ').shift().trim();
+  var message = string.split(' ').splice(1).join(' ').trim();
+
+  pool.forEach( c => {
+    if(c.nickname === nickname) {
+      c.socket.write(`@{client.nickname}: ${message}`);
+    }
+  });
+});
+
+ee.on('@nickname', function(client, string) {
+  let nickname = string.split(' ').shift().trim();
+  client.nickname = nickname;
+  client.socket.write(`user nickname has been changed to ${client.nickname}\n`);
+});
+
+ee.on('default', function(client) {
+  client.socket.write('not a command - please use an @ symbol\n');
+});
+
+server.on('connection', function(socket){
+  var client = new Client(socket);
+  pool.push(client);
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+    if(command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
+      return;
+    }
+    ee.emit('default', client, data.toString());
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('server up:', PORT);
+});
